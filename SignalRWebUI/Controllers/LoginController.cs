e3501c27647cd3c0aad3ae00c36bb5ce9ff9f170
Berat Aktas <berataktas2834@gmail.com>
2024-10-27 16:47:30 +0300

    27.10.2024v2

---
+++ SignalRWebUI/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.EntityLayer.Entities;
+using SignalRWebUI.Dtos.IdentityDto;
+
+namespace SignalRWebUI.Controllers
+{
+	public class LoginController : Controller
+	{
+		private readonly SignInManager<AppUser> _singInManager;
+
+		public LoginController(SignInManager<AppUser> singInManager)
+		{
+			_singInManager = singInManager;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(LoginDto loginDto)
+		{
+			var result = await _singInManager.PasswordSignInAsync(loginDto.Username, loginDto.Password, false, false);
+			if (result.Succeeded)
+			{
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
+
+	}
+}
