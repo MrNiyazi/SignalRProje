7ccaa8182b67700279fc419a61f7f274eea77aef
Berat Aktas <berataktas2834@gmail.com>
2025-02-16 17:16:58 +0300

    16.02.2025-3

---
+++ SignalRWebUI/Controllers/QRCodeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace SignalRWebUI.Controllers
+{
+	public class QRCodeController : Controller
+	{
+		public IActionResult Index(string value)
+		{
+			using (MemoryStream stream = new MemoryStream())
+			{
+				QRCodeGenerator createQRCode = new QRCodeGenerator();
+				QRCodeGenerator.QRCode squareCode = createQRCode.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+				using (Bitmap image = squareCode.GetGraphic(10))
+				{
+					image.Save(stream, ImageFormat.Png);
+					ViewBag.QRCodeImage = "data/png;base64"+Convert.ToBase64String(stream.ToArray());
+				}
+			}
+			return View();
+		}
+	}
+}
