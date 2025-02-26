c22ead3970d5d8938d7be0ad6a352682577a7cb2
Berat Aktas <berataktas2834@gmail.com>
2025-02-26 23:52:03 +0300

    26.02.2025

---
+++ SignalRWebUI/Controllers/MailController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SignalRWebUI.Dtos.MailDtos;
+
+namespace SignalRWebUI.Controllers
+{
+	public class MailController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpDelete]
+		public IActionResult Index(CreateMailDto createMailDto)
+		{
+			MimeMessage mimeMessage = new MimeMessage();
+			MailboxAddress mailboxAddressFrom = new MailboxAddress("SignalR Rezervasyon","mail adresi");
+			mimeMessage.From.Add(mailboxAddressFrom);
+			return RedirectToAction("Index");
+		}
+	}
+}
