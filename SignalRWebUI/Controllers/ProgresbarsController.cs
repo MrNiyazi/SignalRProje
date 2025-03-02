44d421f096297bc3b08aeabf10686f58fe834ffe
Berat Aktas <berataktas2834@gmail.com>
2025-03-02 14:55:57 +0300

    02.03.2025-199

--- SignalRWebUI/Controllers/ProgresbarsController.cs
+++ SignalRWebUI/Controllers/ProgresbarsController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SignalRWebUI.Controllers
 {
 	public class ProgresbarsController : Controller
 	{
+		[AllowAnonymous]
 		public IActionResult Index()
 		{
 			return View();
