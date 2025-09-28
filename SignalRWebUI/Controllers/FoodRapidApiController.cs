713b52f6524e4efb39d238c5bcd5ea575b5390ff
Berat Aktas <berataktas2834@gmail.com>
2025-09-28 20:52:47 +0300

    28.09.2025-Api entegrasyon

--- SignalRWebUI/Controllers/FoodRapidApiController.cs
+++ SignalRWebUI/Controllers/FoodRapidApiController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.RapidApiDtos;
 
 namespace SignalRWebUI.Controllers
 {
 	public class FoodRapidApiController : Controller
 	{
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
-			return View();
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes"),
+				Headers =
+	{
+		{ "x-rapidapi-key", "70dc19646emsh5a00e62444c6f71p1da85cjsnf36a21a19577" },
+		{ "x-rapidapi-host", "tasty.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultTastyApi>>(body);
+				return View(values.ToList());
+				//Console.WriteLine(body);
+			}
+			
 		}
 	}
 }
