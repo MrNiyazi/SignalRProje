97a49c48a478562c983aeb167651d9384aedc4a3
Berat Aktas <berataktas2834@gmail.com>
2025-10-19 19:08:12 +0300

    19.10.2025-Fluent Validation kullanımı

---
+++ SignalR.BusinessLayer/ValidationRules/BookingValidations/CreateBookingValidation.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using SignalR.DtoLayer.BookingDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.ValidationRules.BookingValidations
+{
+	public class CreateBookingValidation : AbstractValidator<CreateBookingDto>
+	{
+		public CreateBookingValidation()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("İsim alanı Boş Geçilemez!");
+			RuleFor(x => x.Phone).NotEmpty().WithMessage("Telefon boş geçilemez!");
+			RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail alanı Boş Geçilemez!");
+			RuleFor(x => x.PersonCount).NotEmpty().WithMessage("Kişi alanı boş geçilemez!");
+			RuleFor(x => x.Date).NotEmpty().WithMessage("Tarih alanı boş geçilemez lütfen tarih seciniz!");
+
+			RuleFor(x => x.Name).MinimumLength(5).WithMessage("Lütfen isim alanına en az 5 karekter veri girişi " +
+				"yapınız").MaximumLength(50).WithMessage("Lütfen isim alanına en fazla 50 karekter veri girişi yapınız");
+			RuleFor(x => x.Description).MaximumLength(500).WithMessage("Lütfen açıklama alanına en fazla 500 karakter veri girişi yapınız.");
+
+			RuleFor(x => x.Mail).EmailAddress().WithMessage("Lütfen geçerli bir mail adresi giriniz");
+		}
+	}
+}
