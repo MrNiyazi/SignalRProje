8ad6b5a72305365a96f4d91c8c335d6ef0976f3e
Berat Aktas <berataktas2834@gmail.com>
2025-10-25 23:44:18 +0300

    25.10.2025

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -83,9 +83,6 @@ builder.Services.AddScoped<IMessageDal, EfMessageDal>();
 builder.Services.AddScoped<IMessageDal, EfMessageDal>();
 
 
-
-
-
 builder.Services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddControllers();
